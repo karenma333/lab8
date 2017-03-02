0843660ed80097bf3f05f4cec17d409345784176
Karen Ma <karenma333@gmail.com>
2017-03-02 10:56:21 -0800

    added partials

--- routes/tasks.js
+++ routes/tasks.js
@@ -8,4 +8,34 @@ var data = {
 exports.view = function(req, res){
   res.render('tasks', data);
 };
+exports.addTask = function(req, res){
+  console.log("hello");
+  var taskName = req.body.taskName;
+  // This will print in your terminal when a POST is made
+  console.log(taskName); 
 
+  data.tasks.push(taskName); // Add to current data
+
+  // Lets send our task name back so our browser knows it worked out!
+  res.send(taskName);
+
+};
+$(document).ready(function() {
+$('#taskForm').submit(function(e) {
+    // Prevents default submit + reload (we only want the submit part)
+    e.preventDefault();
+
+    var taskName = $('#addTask').val();
+
+    // Send the POST request
+    $.post('addTask', { taskName: taskName }, postCallback);
+  });
+});
+
+function postCallback(res) {
+  $('.taskList').append('
+' + res + '
+'); // Add to the list
+
+ $('#addTask').val(''); // Clear form
+}
